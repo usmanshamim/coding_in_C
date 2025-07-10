e851a903eded0e9c1df7b403820c288edf2abba0
usmanshamim <usmanshafqat31@gmail.com>
2025-07-10 16:43:58 +0000

    add test1.c

--- test1.c
+++ test1.c
@@ -7,6 +7,13 @@ example : N=10,   {2 3 4 5 6 7 1 8 10}
 oUTPUT  9
 iNPUT : N=10;  {2 3 4 5 6 7 1 8 10}
 Output 9
+for N=10
+sum = 1+2+3+4+5+6+7+8+9+10 = n(n+1)/2 = (10)(10+1)/2 = 55
+arr = 2 3 4 5 6 7 1 8 10 
+
+XORSUM 
+
+
 */
 #include <stdio.h>
 int main() {
@@ -14,6 +21,17 @@ int main() {
     int arr[]={2,3,4,5,6,7,1,8,10};
     int size=sizeof(arr)/ sizeof(arr[0]);
     int total=N*(N+1)/2;
-    int sum =10;
-    for(int)
+    // int sum =0;
+    int xorsum=0;
+    int i;
+    for( i=0;i<(N-1);i++){
+        // sum+=arr[i];
+        xorsum=xorsum^(i+1)^arr[i];
+        // total=total-arr[i];
+    }
+    // total=total-sum;
+    // printf("\n Missing number = %d\n",total);
+    printf("\n Missing number = %d\n",(xorsum^(i+1)));
+    return 0;
+
 }
